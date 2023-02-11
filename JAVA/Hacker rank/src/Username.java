866c826fe943496ef784e7ae7394c48bf2db06ca
Shubhankargupta691 <shubhankargupta691@gmail.com>
2023-02-11 12:47:17 +0530

    initial commit

--- JAVA/Hacker rank/src/Username.java
+++ JAVA/Hacker rank/src/Username.java
@@ -1,7 +1,23 @@
-import java.util.*;
-public class Username {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class Username {
+
+    private static final Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+
+    class UsernameValidator {
+       public static String regularExpression =  "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
     }
 }
